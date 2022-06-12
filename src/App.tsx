e0e40bba00b2334b58befdcdc9a21586a8d07e66
denis <permwed@gmail.com>
2022-06-12 19:40:53 +0500

    continue work

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,6 @@ import "./static/css/global.css";
 
 console.log(createDate({ locale: "en-US" }));
 
-function test(str: string) {
-  console.log("test");
-}
-function test2(str: string) {
-  console.log("test 2");
-}
-
 const App: FC = () => {
   return <div className="app__container">Calendar</div>;
 };
